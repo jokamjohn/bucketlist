dbe7b49f3d77128b3db71dd9b10da4bde274233f
John Kagga <johnkagga@gmail.com>
2017-07-11 17:25:20 +0300

    Added user registration method to the application class

--- app/app.py
+++ app/app.py
@@ -1,8 +1,24 @@
+"""
+This class handles User registration and login
+"""
+
+
 class Application:
     users = {}
 
     def register_user(self, user):
-        pass
+        """
+        This methods checks whether a user already exists, if not
+        the user is added registered by adding them to the dictionary otherwise
+        False is returned.
+        :param user: 
+        :return: bool
+        """
+        if user.username in self.users.keys():
+            return False
+        else:
+            self.users[user.username] = user
+        return True
 
     def login_user(self, username, password):
         pass
