525ce6fc753e0dd7519637041fda612cb5a0bfe0
John Kagga <johnkagga@gmail.com>
2017-07-11 17:20:34 +0300

    Added tests for user registration and login

---
+++ app/tests/tests.py
@@ -0,0 +1,24 @@
+import unittest
+from app.user import User
+from app.app import Application
+
+
+class TestApplication(unittest.TestCase):
+    def setUp(self):
+        self.user = User('John', 'jokamjohn', '123456')
+        self.app = Application()
+
+    def test_user_is_added_to_dictionary_when_created(self):
+        return self.assertTrue(self.app.register_user(self.user))
+
+    def test_user_already_exists_in_user_dictionary(self):
+        self.app.users = {'jokamjohn': self.user}
+        return self.assertFalse(self.app.register_user(self.user))
+
+    def test_user_sigining_in_is_already_registered(self):
+        self.app.users = {'jokamjohn': self.user}
+        return self.assertTrue(self.app.login_user('jokamjohn', '123456'))
+
+    def test_user_trying_to_login_has_entered_a_wrong_password(self):
+        self.app.users = {'jokamjohn': self.user}
+        return self.assertFalse(self.app.login_user('jokamjohn', 'sdfgdsfj'))
