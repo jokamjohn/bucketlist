11830010c033bf0647f74f5ecd9003fc0d42a1f6
John Kagga <johnkagga@gmail.com>
2017-07-14 12:42:19 +0300

    Configuring heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,12 @@ from flask import Flask
 # Initialize the application
 app = Flask(__name__, instance_relative_config=True)
 
+# App configs
+app.secret_key = 'nbvjlisghruvnxzdnvdlsfgvbfugfvbjsdkvbusdigkbsdjkval'
+app.config['SESSION_TYPE'] = "filesystem"
+
 # Load the views
 from app import views
 
 # Linking the configuration file
 app.config.from_object('config.BaseConfig')
-
-
-
